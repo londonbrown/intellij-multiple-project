c92d3b395acd83ab48b9c5dc5ad708bfc10522de
London Brown <londonbr@amazon.com>
2021-11-04 15:14:55 -0700

    Example gradle-intellij-plugin multiproject build

---
+++ buildSupport/intellijProfiles.groovy
@@ -0,0 +1,44 @@
+environments {
+    "2021_1" {
+        version = '2021.1'
+        plugins = [
+                'java',
+                'gradle',
+                'gradle-java',
+                'Groovy',
+                'properties',
+                'junit',
+                'git4idea',
+                'yaml',
+                'terminal'
+        ]
+        IC_plugins = [
+                'PythonCore:211.6693.119'
+        ]
+        IU_plugins = [
+                'Pythonid:211.6693.115',
+                'org.jetbrains.plugins.ruby:211.6693.111'
+        ]
+    }
+    "2021.2" {
+        version: "2021.2.3"
+        plugins = [
+                'java',
+                'gradle',
+                'gradle-java',
+                'Groovy',
+                'properties',
+                'junit',
+                'git4idea',
+                'yaml',
+                'terminal'
+        ]
+        IC_plugins: [
+                'PythonCore:212.4746.92'
+        ]
+        IU_plugins: [
+                'Pythonid:212.4746.92',
+                'org.jetbrains.plugins.ruby:212.4746.92'
+        ]
+    }
+}
